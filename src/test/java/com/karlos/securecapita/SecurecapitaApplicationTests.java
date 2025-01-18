418335dae163de853cb04c80d61921d560d8ac98
Carlos Rodriguez <carlos.rodriguez1@utp.edu.co>
2025-01-18 12:39:32 -0500

    Initial configuractions, connection and database configuration

---
+++ src/test/java/com/karlos/securecapita/SecurecapitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karlos.securecapita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurecapitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
